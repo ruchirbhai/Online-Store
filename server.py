c88f36f1504929ecd1e533b36e9ce634bc530ad3
ruchirbhai <ruchirbhai@gmail.com>
2020-03-20 23:26:13 -0700

    Initial commit

---
+++ server.py
@@ -0,0 +1,10 @@
+result = application.listen_on('GET product detail on product with id')
+
+print(result.id)   # -> 1
+
+# ... get more info about object with result.id
+# ... we pass the variable data which passes info about result.id
+
+application.render_view('product_detail.html', data)
+
+# The goal int this course is how to populate the data variable
